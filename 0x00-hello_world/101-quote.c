d031e94cd68bedadcf0a4da73547437f25778d8b
CelineAma <ecelineamarachi@yahoo.com>
2022-03-12 05:03:53 -0800

    print a quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program that prints text to standart error
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fprintf(stderr,
+	"and that piece of art is useful\" -Dora Korpa, 2015-10-19\n");
+	return (1);
+}
