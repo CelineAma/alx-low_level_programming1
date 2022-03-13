cf563215d04b01e67219715a04d6a523ac7c6667
CelineAma <ecelineamarachi@yahoo.com>
2022-03-13 07:03:24 -0700

    variable if else while c program

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char alphb;
+
+	for (i = '0'; i <= '9'; i++)
+
+	putchar(i);
+
+	for (alphb = 'a'; alphb <= 'f'; alphb++)
+
+	putchar(alphb);
+	putchar('\n');
+	return (0);
+}
