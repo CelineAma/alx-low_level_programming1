76b4a213160c2e8c47f4d7814b29add4b92e6674
CelineAma <ecelineamarachi@yahoo.com>
2022-04-03 07:29:41 -0700

    Static Libraries in C

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - main entry
+ * @c: check for alphabet character
+ * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _abs - main entry
+* @n: compute absolute value of an integer
+* Return: 0
+*/
+int _abs(int n)
+{
+
+	return (n >= 0 ? n : n * -1);
+
+}
