19d4dffefcc96a1eeee0a1f683e53cfc76f3766a
Celine Amarachi <94835448+CelineAma@users.noreply.github.com>
2022-04-30 00:50:18 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,73 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * free_listint_safe - frees a listint_t list
- * @h: double pointer to head of list
- * Return: number of nodes freed
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_linkl2(listl_k **head)
+{
+	listl_k *tmp;
+	listl_k *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((tmp = current) != NULL)
+		{
+			current = current->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
 {
+	size_t num = 0;
+	listl_k *ptr, *new, *add;
 	listint_t *current;
-	listint_t *next;
-	int diff;
 
-	register short count = 0;
-
-	if (!h || !(*h))
-		return (count);
-	current = *h;
-	while (current)
+	ptr = NULL;
+	while (*h != NULL)
 	{
-		diff = current - current->next;
-		if (diff > 0)
+		new = malloc(sizeof(listl_k));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = ptr;
+		ptr = new;
+
+		add = ptr;
+
+		while (add->next != NULL)
 		{
-			next = current->next;
-			free(current);
-			current = next;
-			count++;
-		} else
-		{
-			free(current);
-			*h = NULL;
-			count++;
-			break;
+			add = add->next;
+			if (*h == add->p)
+			{
+				*h = NULL;
+				free_linkl2(&ptr);
+				return (num);
+			}
 		}
 
+		current = *h;
+		*h = (*h)->next;
+		free(current);
+		num++;
 	}
+
 	*h = NULL;
-	return (count);
+	free_linkl2(&ptr);
+	return (num);
 }
